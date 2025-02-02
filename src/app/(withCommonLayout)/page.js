4346c96cab2bc01124b47aeea29b10e5811a36d6
mdasif8393 <md.asif.8393@gmail.com>
2025-02-02 10:52:20 +0600

    35-5: Explain SSG by Next-level Data Fetching

--- src/app/(withCommonLayout)/page.js
+++ src/app/(withCommonLayout)/page.js
@@ -1,7 +1,9 @@
 import ProductCard from "@/components/Products/ProductCard";
 
 const HomePage = async () => {
-  const res = await fetch("http://localhost:5000/products");
+  const res = await fetch("http://localhost:5000/products", {
+    cache: "force-cache"
+  });
   const products = await res.json();
 
 
